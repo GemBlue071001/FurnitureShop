b205500dc9af3fdf107d0e56393449d260e63bd6
trinhtam <trinhtam2001@gmail.com>
2022-05-12 14:45:51 +0700

    up File

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './component/nav_bar/NavBar'
+import Header from './component/header/Header'
+import Products from './component/Products/Products'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Products />
+    </div>
+  )
+}
+
+export default App
